ce7f06c516154e05f18fd4ccaa808b05e8b1931a
Andrew Cook <andrewjcook93@gmail.com>
2022-11-12 01:11:44 -0600

    fixed navbar to the top of screen. reformatted margins, and maxwidth for nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
+    <nav>
+      <div className="nav-items">
         <Link to="/">HOME</Link>
         <Link to="vehicles">VEHICLES</Link>
         <Link to="about">ABOUT US</Link> 
         <Link to="testimonials">TESTIMONIALS</Link>
         <Link to="contact">CONTACT</Link>
         <Link to="account">ACCOUNT</Link>
-      </nav>
-    </div>
+      </div>
+    </nav>
   )
 }
 
