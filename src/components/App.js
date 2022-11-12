4208caa18fc3d6eb57d5cd5ec724e8001dc27310
Andrew Cook <andrewjcook93@gmail.com>
2022-11-11 21:27:37 -0600

    footer social links and background

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,17 @@ const App = () => {
       <Outlet context />
 
       <footer>
+        <a href="https://www.facebook.com/delavegaautoplex" target="_blank">
+          <img alt="facebook icon link" src="https://i.imgur.com/eTvEDA8.png" target="_blank" />
+        </a>
+        &nbsp;
+        &nbsp;
+        <a href="" target="_blank">
+          <img alt="instagram icon link" src="https://i.imgur.com/fsR5M5g.png" />
+        </a>
+
+        <br />
+        <br />
         &#169; 2019 by Delavega Autoplex.
         <br />
         Developed by Drewford
