d09617d960d13afff65720ea17f245850e5fd4b1
Andrew Cook <andrewjcook93@gmail.com>
2022-11-12 02:12:28 -0600

    trying to fix busted margins and paddings

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const Vehicles = () => {
   return (
-    <div>
-      Vehicles
+    <div id="vehicles-return">
+      <div id="vehicles-container">
+        <div className="vehicle">
+          One
+        </div>
+        <div className="vehicle">
+          Two
+        </div>
+        <div className="vehicle">
+          Three
+        </div>
+        <div className="vehicle">
+          Four
+        </div>
+        <div className="vehicle">
+          Five
+        </div>
+        <div className="vehicle">
+          Six
+        </div>
+        <div className="vehicle">
+          Seven
+        </div>
+      </div>
     </div>
   )
 }
