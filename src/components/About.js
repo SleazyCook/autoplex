911f596e338feac0f6f27d5c051367f3fcc3afa5
Andrew Cook <andrewjcook93@gmail.com>
2022-11-11 23:50:11 -0600

    about background

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div id="about-return">
-      <p id="about-title">ABOUT US</p>
-      <p>Since opening in 2020, Delavega Autoplex has been proud to serve the RGV community. We’re committed to not only providing quality vehicles and service, but going above and beyond to ensure our customers are completely satisfied. We welcome you to look through our site to see all we have to offer.</p>
+    <div className="about-return">
+      hell
+      <div id="about-container">
+        <p id="about-title">ABOUT US</p>
+        <p>Since opening in 2020, Delavega Autoplex has been proud to serve the RGV community. We’re committed to not only providing quality vehicles and service, but going above and beyond to ensure our customers are completely satisfied. We welcome you to look through our site to see all we have to offer.</p>
+      </div>
     </div>
+
   )
 }
 
